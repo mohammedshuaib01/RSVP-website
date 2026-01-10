8cc3484e95e0887c01da0331733b5e1bd3c82116
shuaib <mohammedshuaibka@gmail.com>
2026-01-10 18:54:11 +0530

    new1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,8 +12,8 @@ const Hero = () => {
                 </div>
 
                 <p className="intro-text reveal reveal-up reveal-stagger-1">
-                    With love in their hearts and blessings all around, please<br />
-                    join us for the wedding celebration of
+                    With love in their hearts and blessings all around,<br /> please
+                 join us for the wedding celebration of
                 </p>
 
                 <h1 className="main-names reveal reveal-up reveal-stagger-2">
